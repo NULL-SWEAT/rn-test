5bd42a1481a908c9e353762baddd6fcc10a0f66e
NULL-SWEAT <felipe.094@gmail.com>
2018-04-27 22:05:48 -0300

    Get current position and center on map

--- src/Containers/Map.js
+++ src/Containers/Map.js
@@ -1,31 +1,59 @@
 import React, { Component } from 'react'
-import { View, StyleSheet, PermissionsAndroid, Platform, Geolocation } from 'react-native'
+import { View, StyleSheet, PermissionsAndroid, Platform, Text, ActivityIndicator } from 'react-native'
 import MapView from 'react-native-maps';
 
 export default class Map extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      latitude: null,
+      longitude: null,
+      error: null,
+    }
+  }
+
   static navigationOptions = {
     title: 'react-native-maps',
   }
 
   componentWillMount() {
-    if(Platform.OS === 'android' && !PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION)) {
+    if (Platform.OS === 'android' && !PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION)) {
       this.requestGeolocationPermission()
     }
   }
 
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log(position)
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          error: null,
+        });
+      },
+      (error) => this.setState({ error: error.message }),
+      { enableHighAccuracy: false, timeout: 200000, maximumAge: 1000 },
+    )
+  }
+
   render() {
     return (
-      <View style ={styles.container}>
-        <MapView
-          style={styles.map}
-          region={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.015,
-            longitudeDelta: 0.0121,
-          }}
-        >
-        </MapView>
+      <View style={styles.container}>
+        {this.state.latitude && this.state.longitude ?
+          (
+            <MapView
+              style={styles.map}
+              region={{
+                latitude: this.state.latitude,
+                longitude: this.state.longitude,
+                latitudeDelta: 0.015,
+                longitudeDelta: 0.0121,
+              }}
+            >
+            </MapView>
+          ) : <ActivityIndicator style={styles.loading} size="large" />
+        }
       </View>
     )
   }
@@ -48,12 +76,15 @@ export default class Map extends Component {
 const styles = StyleSheet.create({
   container: {
     ...StyleSheet.absoluteFillObject,
-    height: 500,
-    width: 400,
     justifyContent: 'flex-end',
     alignItems: 'center',
   },
   map: {
     ...StyleSheet.absoluteFillObject,
   },
+  loading: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
