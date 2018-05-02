06d99e5208763f64011a36a454f16b708dc8e1ba
NULL-SWEAT <felipe.094@gmail.com>
2018-05-02 16:54:04 -0300

    Flip front/back camera

--- src/Containers/Camera.js
+++ src/Containers/Camera.js
@@ -11,6 +11,12 @@ import {
 import { RNCamera } from 'react-native-camera';
 
 export default class Camera extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      type: 'back',
+    }
+  }
 
   componentWillMount() {
     PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE)
@@ -21,18 +27,31 @@ export default class Camera extends Component {
       })
   }
 
-  render() {
+  toggleFacing() {
+    this.setState({
+      type: this.state.type === 'back' ? 'front' : 'back',
+    });
+  }
+
+  renderCamera() {
     return (
-      <View style={styles.container}>
-        <RNCamera
+      <RNCamera
             ref={ref => { this.camera = ref; }}
             style = {styles.preview}
-            type={RNCamera.Constants.Type.back}
+            type={this.state.type}
             permissionDialogTitle={'Permission to use camera'}
             permissionDialogMessage={'We need your permission to use your camera phone'}
-        />
+        >
 
         <View style={{flex: 0, flexDirection: 'row', justifyContent: 'center',}}>
+
+          <TouchableOpacity
+              onPress={this.toggleFacing.bind(this)}
+              style = {styles.toggle}
+          >
+              <Text style={{fontSize: 14}}>Flip</Text>
+          </TouchableOpacity>
+
           <TouchableOpacity
               onPress={this.takePicture.bind(this)}
               style = {styles.capture}
@@ -47,18 +66,26 @@ export default class Camera extends Component {
               <Text style={{fontSize: 14}}> Galeria </Text>
           </TouchableOpacity>
         </View>
+      </RNCamera>
+    )
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        {this.renderCamera()}
       </View>
     );
   }
 
   takePicture = async () => {
     if (this.camera) {
-      const options = { quality: 0.5, base64: true };
+      const options = { quality: 1, base64: true }
       const data = await this.camera.takePictureAsync(options)
       // window.alert('base64:' + data.base64)
-      await CameraRoll.saveToCameraRoll(data.uri);
+      await CameraRoll.saveToCameraRoll(data.uri)
     }
-  };
+  }
 
   requestCameraPermission = async () => {
     try {
@@ -85,7 +112,6 @@ const styles = StyleSheet.create({
   preview: {
     flex: 1,
     justifyContent: 'flex-end',
-    alignItems: 'center'
   },
   capture: {
     flex: 0,
@@ -101,6 +127,16 @@ const styles = StyleSheet.create({
     position: 'absolute',
     right: 0,
     backgroundColor: '#FFF',
+    borderRadius: 5,
+    padding: 15,
+    margin: 20,
+  },
+  toggle: {
+    flex:0,
+    position: 'absolute',
+    left: 0,
+    backgroundColor: '#FFF',
+    borderRadius: 5,
     padding: 15,
     margin: 20,
   }
